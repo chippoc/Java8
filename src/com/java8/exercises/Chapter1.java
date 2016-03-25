11a119085b6c3496f69cee0726866fbbad42e25e
Sky Lin <sky.lin@servicenow.com>
2016-03-24 22:41:32 -0700

    finished Chapter 1

--- src/com/java8/exercises/Chapter1.java
+++ src/com/java8/exercises/Chapter1.java
@@ -3,9 +3,14 @@ package com.java8.exercises;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Exercises from Chapter 1.
@@ -23,6 +28,25 @@ public class Chapter1 {
 		}
 	}
 	
+	interface RunnableEx {
+		public void run() throws Exception;
+	}
+	
+	// Problem 6
+	public static Runnable uncheck(RunnableEx runner) {
+		return () -> {
+			try {
+				runner.run();
+			} catch (Exception e) {
+				// ignore all exceptions
+			}
+		};
+	}
+	
+	public static Runnable andThen(Runnable x, Runnable y) {
+		return ()-> {x.run(); y.run();};
+	}
+	
 	public static void main(String[] args) {
 		
 		ArrayList<String> strings = new ArrayList<String>();
@@ -37,7 +61,7 @@ public class Chapter1 {
 		// Problem 1
 		Arrays.sort(args, comp);
 		
-		repeatMessage("Hello", 1000);
+		repeatMessage("Hello", 10);
 		
 		// Problem 2
 		Function<File, File[]> getSubDirs = (File dir) -> {
@@ -55,6 +79,34 @@ public class Chapter1 {
 			if (second.isDirectory() && !first.isDirectory()) return 1;
 			return first.getName().compareTo(second.getName());
 		};
+		
+		// Problem 6
+		new Thread(uncheck(
+				() -> {System.out.println("Zzz"); Thread.sleep(1000); })).start();
+		
+		// Problem 7
+		andThen(()->System.out.println("Go"), ()->System.out.println("Stop")).run();
+		
+		// Problem 8
+		String [] names = {"Peter", "Paul", "Mary"};
+		List<Runnable> runners = new ArrayList<>();
+		for (String name: names)
+			runners.add(()->System.out.println(name));
+		for(Runnable runner: runners)
+			runner.run();
+		
+	}
+	
+	// Problem 9
+	public abstract class Collection2<T> implements Collection<T> {
+		public void forEachIf(Consumer<T> action, Predicate<T> filter) {
+			Iterator<T> iter = iterator();
+			while (iter.hasNext()) {
+				T it = iter.next();
+				if (filter.test(it))
+					action.accept(it);
+			}
+		}
 	}
 	
 	/*
